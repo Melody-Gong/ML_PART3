26015feb96a1844fb1c530638a72972fe9dec356
gongm <go0009in@e.ntu.edu.sg>
2021-01-30 12:58:13 +0800

    test2

--- main.py
+++ main.py
@@ -4,13 +4,21 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import requests
+#url = 'https://data.gov.sg/api/action/datastore_search?resource_id=f9dbfc75-a2dc-42af-9f50-425e4107ae84&limit=5'
+url='https://data.gov.sg/api/action/datastore_search?resource_id=f9dbfc75-a2dc-42af-9f50-425e4107ae84&q=2019'
+result = requests.get(url).json()
+# Try out the following codes
+# Try to understand the result and figure out how is the data
+#stored in json.
+print(result.keys())
+print(result['result'])
 
+#url='https://data.gov.sg/api/action/datastore_search?resource_id=f9dbfc75-a2dc-42af-9f50-425e4107ae84&q=jones'
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+import pandas as pd
+### Replace the highlighted part with json data containing the data records.
+df = pd.DataFrame(result["result"]["records"])
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(df[(df.level_1=="Total Residents")&(df.level_2=="90 Years & Over")])
+
